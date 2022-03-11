58315f9f387ab7facd72fbb8f4a8e173a4c68d49
LeeJooYong <daramzycoder@gmail.com>
2022-03-11 21:57:30 +0900

    feat : 뱀 추가했는데 아직 버그가 많음. 첫번째 뱀이 바로 안움직이고, clearInterval이 제대로 작동안됨. 원인 찾아야함

--- naevis_calling/client/src/components/games/BlackMamba.js
+++ naevis_calling/client/src/components/games/BlackMamba.js
@@ -24,21 +24,23 @@ const Piece = styled.div`
 
 function BlackMamba() {
   const [userPos, setUserPos] = useState([0, 0]);
-  const [snakes, setSnakes] = useState([]);
   const [crystalPos, setCrystalPos] = useState([
     (Math.floor(Math.random() * 100) % 10) + 10,
     (Math.floor(Math.random() * 100) % 10) + 10,
   ]);
   const [score, setScore] = useState(0);
   const [gameState, setGameState] = useState(true);
+  const [snakes, setSnakes] = useState([]);
+  const [snakeChange, setSnakeChange] = useState(0);
   const pieces = Array.from({ length: 25 }, (v) =>
     Array.from({ length: 25 }, (v) => 0)
   );
 
+  const direction = { 0: [-1, 0], 1: [0, 1], 2: [1, 0], 3: [0, -1] };
   let pieceColors = {};
   pieceColors[`${userPos[0]},${userPos[1]}`] = "gray";
   snakes.forEach((snake) => {
-    snake.forEach((el) => {
+    snake.body.forEach((el) => {
       pieceColors[`${el[0]},${el[1]}`] = "black";
     });
   });
@@ -61,6 +63,8 @@ function BlackMamba() {
       setScore(score + 100); // 임시로 100점씩 늘어나게
       crystalRegen(); // 보석을 먹었으니 새로운 위치로 배정
       addSnake(); // 뱀 추가
+    } else if (pieceColors[`${x},${y}`] === "black") {
+      ending();
     }
   };
 
@@ -75,16 +79,27 @@ function BlackMamba() {
   };
 
   const addSnake = () => {
-    // if (snakes.length < 8) {
-    //   const newSnake = [[24,0],[24,1],[24,2],[24,3],[24,4],[24,5],[24,6],[24,7],[24,8]];
-    //   새로운 뱀이 추가될 수 있을 때까지 체크
-    //   checkEmpty();
-    //   추가된 이후로 움직이도록 함
-    //   setSnakes([...snakes, newSnake]);
-    // }
+    if (snakes.length < 8) {
+      const newSnake = {
+        body: [
+          [24, 8],
+          [24, 7],
+          [24, 6],
+          [24, 5],
+          [24, 4],
+          [24, 3],
+          [24, 2],
+          [24, 1],
+          [24, 0],
+        ],
+        dir: 1,
+      };
+      //새로운 뱀이 추가될 수 있을 때까지 체크
+      checkEmpty(newSnake);
+    }
   };
 
-  const checkEmpty = () => {
+  const checkEmpty = (newSnake) => {
     let check = setInterval(function () {
       let flag = true;
       for (let i = 0; i < 9; i++) {
@@ -94,8 +109,57 @@ function BlackMamba() {
       }
       if (flag) {
         clearInterval(check);
+        // 추가될 자리에 아무것도 없으면 뱀 추가
+        setSnakes([...snakes, newSnake]);
+        setSnakeChange(snakeChange + 1);
+        moveSnake(newSnake);
       }
     }, 100);
+  };
+
+  const moveSnake = (snake) => {
+    let forwardCnt = 0;
+    let move = setInterval(function () {
+      const nextHead = [
+        snake.body[0][0] + direction[snake.dir][0],
+        snake.body[0][1] + direction[snake.dir][1],
+      ];
+      if (forwardCnt % 3 === 0) {
+        // 방향 선택
+        const nextDir = Math.floor(Math.random() * 10) % 4;
+        snake.dir = nextDir;
+      } else {
+        // 전진. 갈수 있으면
+        if (
+          nextHead[0] >= 0 &&
+          nextHead[0] < 25 &&
+          nextHead[1] >= 0 &&
+          nextHead[1] < 25
+        ) {
+          if (pieceColors[`${nextHead[0]},${nextHead[1]}`] === "gray") {
+            console.log("Opps");
+            ending();
+          } else if (
+            pieceColors[`${nextHead[0]},${nextHead[1]}`] === undefined
+          ) {
+            snake.body.unshift(nextHead);
+            snake.body.pop();
+          }
+        }
+      }
+      if (gameState === false) {
+        clearInterval(move);
+      }
+      console.log(forwardCnt);
+      forwardCnt += 1;
+      setSnakeChange(snakeChange + 1);
+    }, 100);
+  };
+
+  const ending = () => {
+    setGameState(false);
+    setSnakes([]);
+    console.log("The End!");
   };
 
   return (
